bd9bea73a711d6bb44ab000328ea2848d916b0fd
vi5hnuu <vishnu.12017539@lpu.in>
2023-06-02 23:56:25 +0530

    added some conf for cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
